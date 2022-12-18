3a262fe79bdcb9c1eca6df6a82b92511cd6a3098
Hitesh <hitesh.engineer85@gmail.com>
2022-12-18 23:39:37 +0530

    Eureka Client Registration

--- accounts/src/main/java/com/app/bank/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/app/bank/accounts/AccountsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.app.bank.accounts.repository")
 @ComponentScans({@ComponentScan("com.app.bank.accounts.controller")})
 @EntityScan("com.app.bank.accounts.model")
+@EnableEurekaClient
 public class AccountsApplication {
 
 	public static void main(String[] args) {
