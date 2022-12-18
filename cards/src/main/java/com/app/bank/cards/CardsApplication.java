b9402d27f5f3c93a09c30096ee0073f09e8d2533
Hitesh <hitesh.engineer85@gmail.com>
2022-12-18 23:36:19 +0530

    Eureka Client Registration

--- cards/src/main/java/com/app/bank/cards/CardsApplication.java
+++ cards/src/main/java/com/app/bank/cards/CardsApplication.java
@@ -3,9 +3,11 @@ package com.app.bank.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @RefreshScope
+@EnableEurekaClient
 public class CardsApplication {
 
 	public static void main(String[] args) {
--- loans/src/main/java/com/app/bank/loans/LoansApplication.java
+++ loans/src/main/java/com/app/bank/loans/LoansApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("com.app.bank.loans.model")
 @SpringBootApplication
 @RefreshScope
+@EnableEurekaClient
 public class LoansApplication {
 
 	public static void main(String[] args) {
